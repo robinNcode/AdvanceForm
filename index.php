f481dcae1136b474c94e2fe88305184cf8765a28
robinNcode <msmrobin518@gmail.com>
2021-08-24 16:53:06 +0600

    custom css working

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Demo</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="http://robin.adovasoft.com/cdn/bootstrap54744.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+</head>
+
+<body>
+    <div class="container">
+        <form id="contact" action="#">
+            <div>
+                
+            </div>
+        </form>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="http://robin.adovasoft.com/cdn/js/jquery-3.6.0.min.js"></script>
+    <script src="http://robin.adovasoft.com/cdn/js/popper-2.9.3.min.js"></script>
+    <script src="http://robin.adovasoft.com/cdn/bootstrap.min.js"></script>
+    <script src="http://robin.adovasoft.com/cdn/js/jquery.steps.min.js"></script>
+    <script src="js/advance.js"></script>
+</body>
+
+</html>
